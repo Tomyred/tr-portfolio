a77f48b89d8594cc1eeb2354170a5d9bd334aa8e
Tomyred <tomy.rojo.1@gmail.com>
2021-09-01 20:32:47 -0300

    darkmode

--- src/components/cards-projects/cards-projects.js
+++ src/components/cards-projects/cards-projects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import media from '../../assets/images/media.png'
 import facebook from '../../assets/images/facebook.png'
 import cardForm from '../../assets/images/card-form.png'
+import darkMode from '../../assets/images/dark-mode.png'
 import './cards-projects.css'
 
 function cardsprojects() {
@@ -22,13 +23,19 @@ function cardsprojects() {
           <p>A Facebook clone with Google Authentication and realtime Firestore DB. Leave a comment!</p>
           <a href="https://copybook-df803.web.app/">Take a look</a>
           </div>
-        <div className="card">
+          <div className="card">
+          <img src={darkMode} alt="dark-mode"/>
+          <h4>Theme toggler</h4>
+          <p>A simple page with dynamic theme change by clicking on the sun and the moon</p>
+          <a href="https://tomyred.github.io/Dark-mode/">Take a look</a>
+          </div>
+          <div className="card">
           <img src={media} alt="media"/>
           <h4>Media finder</h4>
           <p>A multimedia content search engine</p>
           <br></br>
           <a href="https://media-finder-75b62.web.app/">Take a look</a>
-          </div> 
+          </div>
           </div>
         </div>
     )
