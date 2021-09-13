35ba283ef98e9b2edfc4e69a80b9e7c84c66874b
Tomyred <tomy.rojo.1@gmail.com>
2021-09-12 23:57:59 -0300

    asd

--- src/components/cards-projects/cards-projects.js
+++ src/components/cards-projects/cards-projects.js
@@ -9,21 +9,21 @@ function cardsprojects() {
     return (
         <div className="component-container">
             <h2>Some of my projects</h2>
-            <p>(More in GitHub repository)</p>
+            <p> (More in GitHub repository)</p>
             <hr></hr>
             <div className="cards-container">
             <div className="card">
             <a href="https://tomyred.github.io/dinamic-card-form/" target="_blank" rel="noreferrer"> 
             <img src={cardForm} alt="dynamic-card-form"/>
             <h4>Credit card form</h4>
-            <p>A dynamic card form with 3d animations that can be used for e-commerce</p>
+            <p className="description">A dynamic card form with 3d animations that can be used for e-commerce</p>
             </a>
           </div>
           <div className="card">
             <a href="https://copybook-df803.web.app/" target="_blank" rel="noreferrer">  
             <img src={facebook} alt="facebook"/>
             <h4>Facebook clone</h4>
-            <p>A Facebook clone with Google Authentication and realtime Firestore DB. Leave a comment!</p>
+            <p className="description">A Facebook clone with Google Authentication and realtime Firestore DB. Leave a comment!</p>
             </a>
           </div>
           {/* <div className="card">
