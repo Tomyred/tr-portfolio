caf68a41fc3eee8e5328bceea6249b8e3d42d744
Tomyred <tomy.rojo.1@gmail.com>
2021-09-13 00:33:09 -0300

    onem

--- src/components/cards-projects/cards-projects.js
+++ src/components/cards-projects/cards-projects.js
@@ -1,8 +1,8 @@
 import React from 'react'
-// import media from '../../assets/images/media.png'
+import media from '../../assets/images/media.png'
 import facebook from '../../assets/images/facebook.png'
 import cardForm from '../../assets/images/card-form.png'
-// import darkMode from '../../assets/images/dark-mode.png'
+//import darkMode from '../../assets/images/dark-mode.png'
 import './cards-projects.css'
 
 function cardsprojects() {
@@ -12,6 +12,7 @@ function cardsprojects() {
             <p> (More in GitHub repository)</p>
             <hr></hr>
             <div className="cards-container">
+
             <div className="card">
             <a href="https://tomyred.github.io/dinamic-card-form/" target="_blank" rel="noreferrer"> 
             <img src={cardForm} alt="dynamic-card-form"/>
@@ -19,6 +20,7 @@ function cardsprojects() {
             <p className="description">A dynamic card form with 3d animations that can be used for e-commerce</p>
             </a>
           </div>
+
           <div className="card">
             <a href="https://copybook-df803.web.app/" target="_blank" rel="noreferrer">  
             <img src={facebook} alt="facebook"/>
@@ -26,19 +28,20 @@ function cardsprojects() {
             <p className="description">A Facebook clone with Google Authentication and realtime Firestore DB. Leave a comment!</p>
             </a>
           </div>
-          {/* <div className="card">
-          <img src={darkMode} alt="dark-mode"/>
+{/* 
+          <div className="card">
+          <a href="https://tomyred.github.io/Dark-mode/" target="_blank" rel="noreferrer"><img src={darkMode} alt="dark-mode"/>
           <h4>Theme toggler</h4>
           <p>A simple page with dynamic theme change by clicking on the sun and the moon</p>
-          <a href="https://tomyred.github.io/Dark-mode/">Take a look</a>
+          </a>
           </div> */}
-          {/* <div className="card">
-          <img src={media} alt="media"/>
+
+          <div className="card">
+          <a href="https://youthful-yonath-701a94.netlify.app/" target="_blank" rel="noreferrer"><img src={media} alt="media"/>
           <h4>Media finder</h4>
-          <p>A multimedia content search engine (movies, shows, books, etc)</p>
-          
-          <a href="https://media-finder-75b62.web.app/">Take a look</a>
-          </div> */}
+          <p>A multimedia content search engine (movies, shows, books, etc) using OMDB API and Bootstrap</p>
+          </a>
+          </div>
           </div>
         </div>
     )
