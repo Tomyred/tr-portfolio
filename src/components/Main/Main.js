5511550c2ce53a400a91262062c4fecbeae3ccb4
Tomyred <tomy.rojo.1@gmail.com>
2021-10-14 17:56:56 -0300

    dsadsa

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,38 +1,95 @@
-import React, { Component } from 'react'
-import './Main.css';
-import Cardsprojects from '../cards-projects/cards-projects';
+import React, { Component } from "react";
+import "./Main.css";
+import Cardsprojects from "../cards-projects/cards-projects";
 
 export default class Main extends Component {
-    render() {
-        return (
-            <div className='componentContainer'>
-            <div className="bigcontainer">
-            <div className="presentation">
-            <h1>Hi! <br/> 
-                I´m
-                <strong> Tomás Rojo MERN Stack Developer</strong> with great passion for learning. <br/>
-                Take a look to some of my <a href="#cards">Projects!</a> </h1>
-            </div>
-            <span></span>
-            <div className="techContainer">
+  render() {
+    return (
+      <div className="componentContainer">
+        <div className="bigcontainer">
+          <div className="presentation">
+            <h1>
+              Hi! <br />
+              I´m
+              <strong> Tomás Rojo MERN Stack Developer</strong> with great
+              passion for learning. <br />
+              Take a look to some of my <a href="#cards">Projects!</a>{" "}
+            </h1>
+          </div>
+          <span></span>
+          <div className="techContainer">
             <div className="technologies">
-                <p id="title">Technologies i work with</p>
-               <div className="iconsContainer">
-                <p>HTML5<img src="https://img.icons8.com/color/48/000000/html-5.png" alt="HTML5"/> </p>
-                <p>CSS3<img src="https://img.icons8.com/color/48/000000/css3.png" alt="CSS"/> </p>
-                <p>JavaScript<img src="https://img.icons8.com/color/48/000000/javascript.png" alt="javascript"/> </p>
-                <p>React<img src="https://img.icons8.com/color/48/000000/react-native.png" alt="react"/> </p>
-                <p>MongoDB<img src="https://img.icons8.com/color/48/000000/mongodb.png" alt="sass"></img></p>
-                <p>SQL<img src="https://img.icons8.com/color/48/000000/sql.png" alt="sql"/> </p>
-                <p>Bootstrap <img src="https://img.icons8.com/color/48/000000/bootstrap.png" alt="bootstrap"></img></p>
-                <p>Git <img src="https://img.icons8.com/color/48/000000/git.png" alt="git"/></p>
-                <p>Github <img src="https://img.icons8.com/color/48/000000/github.png" alt="github"/></p>
-                </div> 
+              <p id="title">Technologies i work with</p>
+              <div className="iconsContainer">
+                <p>
+                  HTML5
+                  <img
+                    src="https://img.icons8.com/color/48/000000/html-5.png"
+                    alt="HTML5"
+                  />{" "}
+                </p>
+                <p>
+                  CSS3
+                  <img
+                    src="https://img.icons8.com/color/48/000000/css3.png"
+                    alt="CSS"
+                  />{" "}
+                </p>
+                <p>
+                  JavaScript
+                  <img
+                    src="https://img.icons8.com/color/48/000000/javascript.png"
+                    alt="javascript"
+                  />{" "}
+                </p>
+                <p>
+                  React
+                  <img
+                    src="https://img.icons8.com/color/48/000000/react-native.png"
+                    alt="react"
+                  />{" "}
+                </p>
+                <p>
+                  MongoDB
+                  <img
+                    src="https://img.icons8.com/color/48/000000/mongodb.png"
+                    alt="sass"
+                  ></img>
+                </p>
+                <p>
+                  SQL
+                  <img
+                    src="https://img.icons8.com/color/48/000000/sql.png"
+                    alt="sql"
+                  />{" "}
+                </p>
+                <p>
+                  Bootstrap{" "}
+                  <img
+                    src="https://img.icons8.com/color/48/000000/bootstrap.png"
+                    alt="bootstrap"
+                  ></img>
+                </p>
+                <p>
+                  Git{" "}
+                  <img
+                    src="https://img.icons8.com/color/48/000000/git.png"
+                    alt="git"
+                  />
+                </p>
+                <p>
+                  Github{" "}
+                  <img
+                    src="https://img.icons8.com/color/48/000000/github.png"
+                    alt="github"
+                  />
+                </p>
+              </div>
             </div>
-            </div>
-            </div>
-            <Cardsprojects/>
-            </div>
-        )
-    }
+          </div>
+        </div>
+        <Cardsprojects />
+      </div>
+    );
+  }
 }
--- src/components/cards-projects/cards-projects.js
+++ src/components/cards-projects/cards-projects.js
@@ -1,62 +1,96 @@
-import React from 'react'
-import media from '../../assets/images/media.png'
-import facebook from '../../assets/images/facebook.png'
-import cardForm from '../../assets/images/card-form.png'
-import crypto from '../../assets/images/crypto.png'
-import darkMode from '../../assets/images/dark-mode.png'
-import './cards-projects.css'
-import Contact from '../Contact/Contact'
+import React from "react";
+import media from "../../assets/images/media.png";
+import facebook from "../../assets/images/facebook.png";
+import cardForm from "../../assets/images/card-form.png";
+import crypto from "../../assets/images/crypto.png";
+import darkMode from "../../assets/images/dark-mode.png";
+import "./cards-projects.css";
+import Contact from "../Contact/Contact";
 
 function cardsprojects() {
-  
-    return (
-      <div>
-        <div className="component-container" id='cards'>
-            <h2>Some of my projects</h2>
-            <div className="cards-container">
-
+  return (
+    <div>
+      <div className="component-container" id="cards">
+        <h2>Some of my projects</h2>
+        <div className="cards-container">
           <div className="card">
-            <a href="https://tomyred.github.io/dinamic-card-form/" target="_blank" rel="noreferrer"> 
-            <img src={cardForm} alt="dynamic-card-form"/>
-            <h4>Credit card form</h4>
-            <p className="description">A dynamic card form with 3d animations that can be used for e-commerce</p>
+            <a
+              href="https://tomyred.github.io/dinamic-card-form/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={cardForm} alt="dynamic-card-form" />
+              <h4>Credit card form</h4>
+              <p className="description">
+                A dynamic card form with 3d animations that can be used for
+                e-commerce
+              </p>
             </a>
           </div>
 
           <div className="card">
-            <a href="https://copybook-df803.web.app/" target="_blank" rel="noreferrer">  
-            <img src={facebook} alt="facebook"/>
-            <h4>Facebook clone</h4>
-            <p className="description">(Deprecated) A Facebook clone with Google Authentication and realtime Firestore DB. Leave a comment!</p>
+            <a
+              href="https://copybook-df803.web.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={facebook} alt="facebook" />
+              <h4>Facebook clone</h4>
+              <p className="description">
+                A Facebook clone with Google Authentication and realtime
+                Firestore DB. Leave a comment!
+              </p>
             </a>
           </div>
 
           <div className="card">
-            <a href="https://tomyred.github.io/Dark-mode/" target="_blank" rel="noreferrer">  
-            <img src={darkMode} alt="facebook"/>
-            <h4>Dark-Mode</h4>
-            <p className="description">A simple theme to toggle by clicking on the sun and the moon.</p>
+            <a
+              href="https://tomyred.github.io/Dark-mode/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={darkMode} alt="facebook" />
+              <h4>Dark-Mode</h4>
+              <p className="description">
+                A simple theme to toggle by clicking on the sun and the moon.
+              </p>
             </a>
           </div>
 
           <div className="card">
-          <a href="https://affectionate-bell-7e3879.netlify.app/" target="_blank" rel="noreferrer"><img src={crypto} alt="crypto"/>
-          <h4>Cryptocurrency prices</h4>
-          <p>Bootstrap interface of current cryptocurrency prices using the Coingecko api</p>
-          </a>
-          </div> 
+            <a
+              href="https://affectionate-bell-7e3879.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={crypto} alt="crypto" />
+              <h4>Cryptocurrency prices</h4>
+              <p>
+                Bootstrap interface of current cryptocurrency prices using the
+                Coingecko api
+              </p>
+            </a>
+          </div>
 
           <div className="card">
-          <a href="https://youthful-yonath-701a94.netlify.app/" target="_blank" rel="noreferrer"><img src={media} alt="media"/>
-          <h4>Media finder</h4>
-          <p>A multimedia content search engine (movies, shows, books, etc) using OMDB API and Bootstrap</p>
-          </a>
-          </div>
+            <a
+              href="https://youthful-yonath-701a94.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={media} alt="media" />
+              <h4>Media finder</h4>
+              <p>
+                A multimedia content search engine (movies, shows, books, etc)
+                using OMDB API and Bootstrap
+              </p>
+            </a>
           </div>
         </div>
-        <Contact/>
-        </div>
-    )
+      </div>
+      <Contact />
+    </div>
+  );
 }
 
-export default cardsprojects
+export default cardsprojects;
