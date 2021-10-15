ca4f97d7c671f621b576499695bd09e55d82710c
Tomyred <tomy.rojo.1@gmail.com>
2021-10-14 22:20:56 -0300

    contact changes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,37 +1,45 @@
-import React, { Component } from 'react'
-import './Contact.css'
-import contact from './../../assets/images/contact.png'
+import React, { Component } from "react";
+import "./Contact.css";
+import contact from "./../../assets/images/contact.png";
 // import picture from './../../assets/images/tomi.jpg'
 
-
 export default class Contact extends Component {
-    render() {
-        return (
-                
-                    <div className="contactContainer">
-                    
-                    <div className="socialmediacontainer">
-                    <div className="contactHeader">
-                    <img id="contactLogo" src={contact} alt="aliencontact"/>
-                    <h1>Contact</h1>
-                    </div>
-                    <div className="mediaLinks">
-                        <div className="linksTop">
-                          <p><img src="https://img.icons8.com/fluency/48/000000/gmail.png" alt="gmail"/> Email: tom.roj.laboral@gmail.com </p>
-                    <p> <img src="https://img.icons8.com/fluency/48/000000/linkedin.png" alt="linkedin"/> LinkedIn: <a href="https://linkedin.com/in/tomás-rojo-38743a214" target="_blank" rel="noreferrer">https://linkedin.com/in/tomás-rojo-38743a214</a></p>  
-                        </div>
-                        <div className="linksBottom">
-                        <p> <img src="https://img.icons8.com/color/48/000000/github.png" alt="github"/> GitHub: <a href="https://github.com/Tomyred" target="_blank" rel="noreferrer">https://github.com/Tomyred</a></p>
-                    <p><img src="https://img.icons8.com/fluency/48/000000/instagram-new.png" alt="instagram" /> Instagram: <a href="https://www.instagram.com/tomas.rojo.1/" target="_blank" rel="noreferrer">https://www.instagram.com/tomas.rojo.1/</a> </p>
-                        </div>
-                    
-                    </div>
-                    </div>
-                    </div>    
-                
-                
-                
-    
-        )
-    }
+  render() {
+    return (
+      <div className="socialmediacontainer">
+        <div className="contactHeader">
+          <img id="contactLogo" src={contact} alt="aliencontact" />
+          <h1>Contact</h1>
+        </div>
+        <div className="mediaLinks">
+          <a
+            href="www.linkedin.com/in/tomás-rojo-38743a214"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/fluency/48/000000/linkedin.png"
+              alt="linkedin"
+            />
+          </a>
+          <a href="https://github.com/Tomyred" target="_blank" rel="noreferrer">
+            <img
+              src="https://img.icons8.com/color/48/000000/github.png"
+              alt="github"
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/tomas.rojo.1/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/fluency/48/000000/instagram-new.png"
+              alt="instagram"
+            />
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/cards-projects/cards-projects.js
+++ src/components/cards-projects/cards-projects.js
@@ -88,6 +88,7 @@ function cardsprojects() {
           </div>
         </div>
       </div>
+      <hr />
       <Contact />
     </div>
   );
